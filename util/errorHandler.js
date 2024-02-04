05f81833c9aae2bab8589f10107558d4b6b1aa90
christopher.totanes <christopher.totanes@gmail.com>
2024-02-04 22:10:10 +0800

    Refactor errorHandler to swich case logic

--- util/errorHandler.js
+++ util/errorHandler.js
@@ -1,45 +1,45 @@
 export function errorHandler(error, res) {
-    if (error.message === "Invalid input") {
-        res.status(400).send("Invalid input");
+    let statusCode = 500;
+    let errorMessage = "";
 
-    } else if (error.message === "Unidentified field") {
-        res.status(400).send("Unidentified field");
-
-    } else if (error.message === "User not found") {
-        res.status(404).send("User not found");
-
-    } else if (error.message === "Unauthorized access") {
-        res.status(403).send("Unauthorized access");
-
-    } else if (error.message === "Incorrect id") {
-        res.status(400).send("Incorrect id");
-
-    } else if (error.message === "Same password") {
-        res.status(409).send("Same password");
-
-    } else if (error.message === "Shop not found") {
-        res.status(404).send("Shop not found");
-
-    } else if (error.message === "Update unsuccessful") {
-        res.status(400).send("Update unsuccessful");
-
-    } else if (error.message === "Create Unsuccessful") {
-        res.status(400).send("Create Unsuccessful");
-
-    } else if (error.code === "23505") {
-        res.status(409).send("Username or Email already exists");
+    switch (error.message) {
+        case "Invalid input":
+        case "Unidentified field":
+        case "Incorrect id":
+        case "Update unsuccessful":
+        case "Create Unsuccessful":
+        case "Input empty":
+        case "Max length reached":
+        case "Input is Null or Undefined":
+            statusCode = 400;
+            errorMessage = error.message;
+            break;
         
-    } else if (error.message === "Input empty") {
-        res.status(400).send("Input empty");
+        case "Unauthorized access":
+            statusCode = 403;
+            errorMessage = error.message;
+            break;
 
-    } else if (error.message === "Max length reached") {
-        res.status(400).send("Max length reached");
-
-    } else if (error.message === "Input is Null or Undefined") {
-        res.status(400).send("Input is Null or Undefined");
-
-    } else {
-        res.status(500).send("Internal Server Error");
+        case "User not found":
+        case "Shop not found":
+            statusCode = 404;
+            errorMessage = error.message;
+            break;
         
-    } 
+        case "Same password":
+            statusCode = 409;
+            errorMessage = error.message;
+            break;
+    
+        default:
+            if (error.code === "23505") {
+                statusCode = 409;
+                errorMessage = "Username or Email already registered";
+            } else {
+                errorMessage = `Internal Server Error: ${error.message}`;
+            }
+            break;
+    }
+
+    res.status(statusCode).send({ error: errorMessage });
 };
