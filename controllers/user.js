20bc48c9aa23c5f680c5084a21a9667aa9274ca1
christopher.totanes <christopher.totanes@gmail.com>
2024-03-01 23:46:52 +0800

    Refactored payload of res.send of loginUser function in user controller

--- controllers/user.js
+++ controllers/user.js
@@ -2,58 +2,65 @@ import { createAccessToken } from "../auth.js";
 import User from "../Models/User.js";
 import { errorHandler } from "../util/errorHandler.js";
 
-export async function getUsers (req, res) {
-	try {
-		const users = await User.getAll();
-		res.status(200).json(users);
-	} catch (error) {
-		errorHandler(error, res);
-	}
-};
+export async function getUsers(req, res) {
+  try {
+    const users = await User.getAll();
+    res.status(200).json(users);
+  } catch (error) {
+    errorHandler(error, res);
+  }
+}
 
-export async function getUserById (req, res) {
-	try {
-		const user = await User.getById(req.params.id);
-		res.status(200).json(user);
-	} catch (error) {
-		errorHandler(error, res);
-	}
-};
+export async function getUserById(req, res) {
+  try {
+    const user = await User.getById(req.params.id);
+    res.status(200).json(user);
+  } catch (error) {
+    errorHandler(error, res);
+  }
+}
 
-export async function registerUser (req, res) {
-	try {
-		const newUser = await User.create(req.body);
-		if (newUser) {
-			res.status(201).json(newUser);
-		}
-	} catch (error) {
-		errorHandler(error, res);
-	}
-};
+export async function registerUser(req, res) {
+  try {
+    const newUser = await User.create(req.body);
+    if (newUser) {
+      res.status(201).json(newUser);
+    }
+  } catch (error) {
+    errorHandler(error, res);
+  }
+}
 
-export async function loginUser (req, res) {
-	try {
-		const userFound = await User.login(req.body);
-		res.status(201).send({ access: createAccessToken(userFound) });
-	} catch (error) {
-		errorHandler(error, res);
-	}
-};
+export async function loginUser(req, res) {
+  try {
+    const userFound = await User.login(req.body);
+    res.status(201).send({
+      user: userFound.username,
+      token: createAccessToken(userFound),
+    });
+  } catch (error) {
+    errorHandler(error, res);
+  }
+}
 
-export async function changePassword (req, res) {
-	try{
-		const updateResult = await User.changePW(req.params.id, req.body.password, req.user);
-		res.sendStatus(200);
-	} catch (error){
-		errorHandler(error, res);
-	}
-};
+export async function changePassword(req, res) {
+  try {
+    const updateResult = await User.changePW(
+      req.params.id,
+      req.body.password,
+      req.user
+    );
+    res.sendStatus(200);
+  } catch (error) {
+    errorHandler(error, res);
+  }
+}
 
-export async function changeUserToAdmin (req, res){
-	try{
-		await User.toAdmin(req.params.id);
-		res.send(204);
-	} catch (error){
-		errorHandler(error, res);
-	}
-};
+export async function changeUserToAdmin(req, res) {
+  try {
+    await User.toAdmin(req.params.id);
+    res.send(204);
+  } catch (error) {
+    errorHandler(error, res);
+  }
+}
